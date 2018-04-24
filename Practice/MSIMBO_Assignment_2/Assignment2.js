0ae621eed635fdc18438178161a44315e6922a17
TSN Student <student@ulem.org>
2018-04-24 15:59:17 -0400

    April 24 Updates

--- Practice/MSIMBO_Assignment_2/Assignment2.js
+++ Practice/MSIMBO_Assignment_2/Assignment2.js
@@ -5,7 +5,22 @@
 // Questions
 // 1.	Write the function printInt(n) taking one parameter n and print all natural numbers from 1 to n in console.
 
+function printInt(n){}
+
+// function printInt(num){
+// 	// var num = 1;
+// 	for (var num=0; num<=100; num++){
+// 		document.write(function printInt(num));
+// 	}
+
+// }
+	
+
+
 // 2.	Write the function printIntRev(n) taking one parameter n and  print all natural numbers in reverse (from n to 1) in console.
+
+// function printIntRev(n){}
+
 
 // 3.	Write the function checkInput(x) taking one parameter x and return the string ‘number’ if x is a number; return the string ‘string’ if x is a string; and return boolean if x is a boolean. Otherwise returns -1. 
 // E.g. checkInput(5) => ‘number’
@@ -14,12 +29,16 @@
 //        checkInput(true)=> boolean
 //        checkInput(none) => -1
 
+// function checkInput(x){}
+
 // 4.	Write the function simpleEvenAdding(num) taking a number and add up all the even numbers from 1 to num. 
 // 	Examples: simpleEvenAdding(5) => 6 (because 2+4 = 6)
 // 	       simpleEvenAdding(10) => 30 (because 2+4+6+8+10 = 30)
 // 	       simpleEvenAdding(11) => 6 (because 2+4+6+8+10 = 30)
 //                   simpleEvenAdding(1) => 0
 // 	       simpleEvenAdding(0) => 0
+
+// simpleEvenAdding(num){}
 
 // 5.	Write the function letterCapitalize(str) taking a string and capitalizaed the first letter of each word. The given words will be separated by only one space.
 // 	Examples:
@@ -28,12 +47,16 @@
 	
 // Hint: There is a built-in function in javaScript that change string into uppercase. There is also function to separate string into an array of characters.
 
+// function letterCapitalize(str){}
+
 // 6.	Write the function simpleReverse(str) taking a string and return the string in reversed order. 
 // 	Examples:
 // 		simpleReverse(‘hello’) => ‘olleh’
 // 		simpleReverse(‘I Love Code’) => ‘edoC evoL I’
 
 // Hint: Think of how you can loop through a string or array of characters backwards to produce a new string
+
+// function simpleReverse(str){}
 
 // 7.	Write the function findDiff(arr) taking a array of numbers as parameter and return the difference between the maximum number and the minimum number (max - min). 
 // 	Examples:
@@ -43,6 +66,8 @@
 // findDiff([1]) => 0;
 // findDiff([]) => 0;
 
+// function findDiff(arr){}
+
 // 8.	Write the function timeConvert(num) taking a number as parameter and return the number of hours and minutes the parameter converts to. Seperate the number of hours and minutes with a colon.
 // 	Example:
 // 		timeConvert(61) => 1:1
@@ -51,11 +76,15 @@
 // 		timeConvert(59)=> 0:59
 //     	Hints: Dividing and modulo the number 60.
 
+// function timeConvert(num){}
+
 // 9.	Write the function findStr(str, long) taking two strings as parameters and return how many str you can find in long. Assume Str is not empty string.
 // Example:
 // 	findStr(“hi”, “dasdhidasdahidashi”) => 3
 // 	findStr(“o”, “daodo”) => 2
 // 	findStr(“ha”,”abcde”)=> 0
+
+// function findStr(str, long){}
 
 // 10.	Write the function selfDividingNumbers(left, right) taking two number bound as parameters and returns an array of every possible self dividing number between 
 // them, including the bounds. 
@@ -67,6 +96,7 @@
 // 	selfDividingNumbers(1, 10) => [1,2,3,4,5,6,7,8,9]
 // selfDividingNumbers(12, 21) => [12, 15]
 
+// function selfDividingNumbers(left, right){}
 
 // Extra Credit:
 // Write the function moveZeros(nums) taking an arry of numbers and move all 0’s to the end of it while maintaining the relative order of the non-zero elements. For example:
@@ -74,6 +104,8 @@
 // moveZeros([1,2,0,0,2,312,33,0,0]=>[1,2,2,312,33]
 // moveZeros([0,0,0])=>[0,0,0]
 // moveZeros([1,2,312,11,2]) => [1,2,312,11,2]
+
+// function moveZeros(nums){}
 
 // Deliverables
 // 	Send your assignment2.js while email to shiyu3169@gmail.com
